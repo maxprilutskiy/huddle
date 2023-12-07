7a3b3a1b9560f80790ef12810038efa8d26e8a45
maxprilutskiy <maks.prilutskiy@gmail.com>
2023-12-07 12:43:13 -0500

    feat: add ai.py with draft code

---
+++ api/ai.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Now you can access the variables defined in .env
+test_env_var = os.getenv('GOOGLE_VERTEX_AUTH')
+
+# log the value of the variable
+print(test_env_var)
+
+# function that accepts an arbitrary json object and returns embeddings as a list of floats
+def get_embeddings(json_object):
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
