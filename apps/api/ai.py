9ce2f1d5554da324264947917aec69b0db228feb
VeranikaP <veranika.prilutskaya@gmail.com>
2023-12-07 18:35:51 -0500

    feat: get_similar implementation

--- apps/api/ai.py
+++ apps/api/ai.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from llama_index.embeddings import GooglePaLMEmbedding
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility, MilvusClient
 import json
 import os
 
@@ -8,9 +9,12 @@ load_dotenv()
 
 # Now you can access the variables defined in .env
 test_env_var = os.getenv('GOOGLE_VERTEX_AUTH')
-filename = 'input.json'
+database = 'data.json'
+input_data = 'input.json'
 model_name = "models/embedding-gecko-001"
 google_cloud_api_key = os.getenv('GOOGLE_CLOUD_API_KEY')
+CLUSTER_ENDPOINT = 'https://in03-2a6bab9a8184f32.api.gcp-us-west1.zillizcloud.com'
+TOKEN = 'b14a398b0399112f1393cd56ea2aac84c4a133c8a1abe0310668892d8fd68acfa6004ba0d58ad2b8130e5c892a32d9810ef2090d'
 embed_model = GooglePaLMEmbedding(model_name=model_name, api_key=google_cloud_api_key)
 
 # log the value of the variable
@@ -51,7 +55,20 @@ def get_embeddings(filename):
         file_counter += 1
     
 # function that accepts an embedding and returns a list of similar embeddings
-def get_similar(embedding):
-    return [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
+def get_similar(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    client = MilvusClient(
+    uri=CLUSTER_ENDPOINT, # Cluster endpoint obtained from the console
+    token=TOKEN # API key or a colon-separated cluster username and password
+    )
+    results = client.search(
+    data=[data["rows"][0]["vector"]],
+    anns_field="vector"
+    )
+    print(results)
 
-get_embeddings(filename)
+# get_embeddings(filename)
+get_similar(input_data)
+
+
