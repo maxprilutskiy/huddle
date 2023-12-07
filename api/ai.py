a8a81e9c82fc570892465db80f5fec716fadf5af
VeranikaP <veranika.prilutskaya@gmail.com>
2023-12-07 14:45:07 -0500

    feat: generate embeddings from the text

--- api/ai.py
+++ api/ai.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+from llama_index.llms.vertex import Vertex
+from llama_index.embeddings import GooglePaLMEmbedding
+import json
 import os
 
 # Load the .env file
@@ -6,14 +9,44 @@ load_dotenv()
 
 # Now you can access the variables defined in .env
 test_env_var = os.getenv('GOOGLE_VERTEX_AUTH')
+filename = 'data.json'
+model_name = "models/embedding-gecko-001"
+google_cloud_api_key = os.getenv('GOOGLE_CLOUD_API_KEY')
+embed_model = GooglePaLMEmbedding(model_name=model_name, api_key=google_cloud_api_key)
 
 # log the value of the variable
 print(test_env_var)
 
 # function that accepts an arbitrary json object and returns embeddings as a list of floats
-def get_embeddings(json_object):
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+def get_embeddings(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    
+    # Initialize the list to store embeddings
+    embeddings_list = []
+    
+    # Extract text data and generate embeddings
+    for entry in data:
+        location = entry.get('location', '')
+        company_description = entry.get('company_description', '')
+        latest_achievement = entry.get('latest_achievement', '')
+        current_challenge = entry.get('current_challenge', '')
+        
+        # Concatenate text data
+        text_to_embed = f"{location} {company_description} {latest_achievement} {current_challenge}"
+        
+        #Generate embeddings for the concatenated text
+        embeddings = embed_model.get_text_embedding(text_to_embed)
+        
+        # Append embeddings to the list
+        embeddings_list.append(embeddings)
+    
+    # Now embeddings_list contains embeddings for each entry in your JSON
+    # You can use embeddings_list for further processing or analysis
+    print(embeddings_list)
+    
 # function that accepts an embedding and returns a list of similar embeddings
 def get_similar(embedding):
     return [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
+
+get_embeddings('data.json')
