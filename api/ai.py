13e70709340c301e730e19b47292dea263b0808e
maxprilutskiy <maks.prilutskiy@gmail.com>
2023-12-07 12:48:41 -0500

    feat: added draft pythong ai funcs

--- api/ai.py
+++ api/ai.py
@@ -13,3 +13,7 @@ print(test_env_var)
 # function that accepts an arbitrary json object and returns embeddings as a list of floats
 def get_embeddings(json_object):
     return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# function that accepts an embedding and returns a list of similar embeddings
+def get_similar(embedding):
+    return [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
