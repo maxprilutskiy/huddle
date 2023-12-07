ec5e8af7f1228217e1f356bc03f31f867967cff8
maxprilutskiy <maks.prilutskiy@gmail.com>
2023-12-07 16:00:47 -0500

    feat: add two endpoints

--- apps/api/app.py
+++ apps/api/app.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # This function will run when the "/" endpoint is hit
-    # It returns a JSON response with a greeting
-    return jsonify(message='Hello, World!')
+@app.route('/create-embeddings', methods=['POST'])
+def get_embeddings():
+    data = request.get_json()
+    input_text = data.get('input', None)
+    if input_text is None:
+        return jsonify({'error': 'No input field provided'}), 400
+    # TODO: Process the input_text to generate embeddings
+    embeddings = []
+    return jsonify({'embeddings': embeddings})
+
+@app.route('/get-similar', methods=['POST'])
+def get_similar():
+    data = request.get_json()
+    input_numbers = data.get('input', None)
+    if input_numbers is None or not isinstance(input_numbers, list):
+        return jsonify({'error': 'No input field provided or input is not an array'}), 400
+    # TODO: Process the input_numbers to find similar items
+    ids = []
+    return jsonify({'ids': ids})
 
 if __name__ == '__main__':
     # The application runs on the port provided by Heroku or 3000 if it's not set
