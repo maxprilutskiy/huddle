3868f4f077ad3483e85eaad56bd17a5ba2fb9fc7
VeranikaP <veranika.prilutskaya@gmail.com>
2023-12-07 20:07:21 -0500

    feat: search similar embeddings

--- apps/api/ai.py
+++ apps/api/ai.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from llama_index.embeddings import GooglePaLMEmbedding
-from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility, MilvusClient
+import requests
 import json
 import os
 
@@ -8,17 +8,25 @@ import os
 load_dotenv()
 
 # Now you can access the variables defined in .env
-test_env_var = os.getenv('GOOGLE_VERTEX_AUTH')
 database = 'data.json'
 input_data = 'input.json'
-model_name = "models/embedding-gecko-001"
+input_vector = 'embedding_1.json'
+
+
 google_cloud_api_key = os.getenv('GOOGLE_CLOUD_API_KEY')
-CLUSTER_ENDPOINT = 'https://in03-2a6bab9a8184f32.api.gcp-us-west1.zillizcloud.com'
-TOKEN = 'b14a398b0399112f1393cd56ea2aac84c4a133c8a1abe0310668892d8fd68acfa6004ba0d58ad2b8130e5c892a32d9810ef2090d'
+
+zilliz_public_endpoint = os.getenv('ZILLIZ_ENDPOINT')
+zilliz_token = os.getenv('ZILLIZ_TOKEN')
+
+model_name = "models/embedding-gecko-001"
 embed_model = GooglePaLMEmbedding(model_name=model_name, api_key=google_cloud_api_key)
 
-# log the value of the variable
-print(test_env_var)
+# URL and headers
+url = f'https://{zilliz_public_endpoint}/v1/vector/search'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {zilliz_token}'
+}
 
 # function that accepts an arbitrary json object and returns embeddings as a list of floats
 def get_embeddings(filename):
@@ -58,17 +66,19 @@ def get_embeddings(filename):
 def get_similar(filename):
     with open(filename, 'r') as json_file:
         data = json.load(json_file)
-    client = MilvusClient(
-    uri=CLUSTER_ENDPOINT, # Cluster endpoint obtained from the console
-    token=TOKEN # API key or a colon-separated cluster username and password
-    )
-    results = client.search(
-    data=[data["rows"][0]["vector"]],
-    anns_field="vector"
-    )
-    print(results)
+        
+    print(data["rows"][0]["vector"])
+    json_payload = json.dumps({"collectionName": "UserProfile",'vector':data["rows"][0]["vector"]})
+    response = requests.post(url, headers=headers, data=json_payload)
+    # Check the response
+    if response.status_code == 200:
+        print("Request successful")
+        print(response.json())  # This will display the response data
+    else:
+        print("Request failed with status code:", response.status_code)
+        print(response.text)  # This will show the error message or details
 
 # get_embeddings(filename)
-get_similar(input_data)
+# get_similar(input_vector)
 
 
